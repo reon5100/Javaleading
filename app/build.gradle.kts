bcf2294c04e0388db235eb78d00ecac08237e3d3
reon5100 <141133756+reon5100@users.noreply.github.com>
2023-09-11 14:18:22 +0900

    グラフ化まで出来たよ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,10 +29,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildToolsVersion = "30.0.3"
 }
 
 dependencies {
-
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/websarva/wings/adroid/javaleading/MainActivity.java
+++ app/src/main/java/com/websarva/wings/adroid/javaleading/MainActivity.java
@@ -6,31 +6,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int YOUR_REQUEST_CODE = 123;
     private ActivityResultLauncher<Intent> filePickerLauncher;
     private TextView ResultText;
+    private LineChart ChartPPGdc;
+    private static final int Bt_select = R.id.bt_Lead;
+    private static final int Bt_Reset = R.id.bt_Reset;
+    private static final int yousosuu = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ChartPPGdc = findViewById(R.id.chart);
+        XAxis xAxis = ChartPPGdc.getXAxis();
+        YAxis yAxis = ChartPPGdc.getAxisLeft();
+        yAxis.setAxisMinimum(0f);
         ResultText =findViewById(R.id.tv_result);
         Button selectFileButton = findViewById(R.id.bt_Lead);
+        Button resetFileButton = findViewById(R.id.bt_Reset);
         selectListener listener = new selectListener();
         selectFileButton.setOnClickListener(listener);
+        resetFileButton.setOnClickListener(listener);
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("*/*"); // すべてのファイルタイプを許可
@@ -48,11 +69,17 @@ public class MainActivity extends AppCompatActivity {
     private class selectListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-            intent.addCategory(Intent.CATEGORY_OPENABLE);
-            intent.setType("*/*");
-
-            filePickerLauncher.launch(intent);
+            int Id =view.getId();
+            if(Id == Bt_select) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("text/plain");
+                filePickerLauncher.launch(intent);
+            }
+            else if(Id == Bt_Reset){
+                ChartPPGdc.notifyDataSetChanged();
+                ChartPPGdc.invalidate();
+            }
         }
     }
     private void displayTextFromFile(Uri fileUri) {
@@ -67,9 +94,103 @@ public class MainActivity extends AppCompatActivity {
             }
             inputStream.close();
             String fileContents = stringBuilder.toString();
-            ResultText.setText(fileContents);
+            String[] dataArray = fileContents.split("\n+|,");
+            List<Integer> numbersList = new ArrayList<>();
+            for (String data : dataArray) {
+                try {
+                    int number = Integer.parseInt(data.trim()); // 文字列を整数に変換
+                    numbersList.add(number);
+                } catch (NumberFormatException e) {
+                    // 数字でないデータは無視
+                }
+
+            }
+
+            // numbersListを配列に変換
+            Integer[] numbersArray = numbersList.toArray(new Integer[0]);
+            int count1 = 0;
+            int Msec=0,Pressure=0,PPGAc=0,PPGDc=0,PPGRaw=0;
+            int[] msec = new int[yousosuu];
+            int[] pressure = new int[yousosuu];
+            int[] PPGac = new int[yousosuu];
+            int[] PPGdc= new int[yousosuu];
+            int[] PPGraw= new int[yousosuu];
+            for (int number :numbersArray){
+                switch (count1%9){
+                    case 0:
+                        msec[Msec]+=(number*86400000);
+                        break;
+                    case 1:
+                        msec[Msec]+=(number*3600000);
+                        break;
+                    case 2:
+                        msec[Msec]+=(number*60000);
+                        break;
+                    case 3:
+                        msec[Msec]+=(number*1000);
+                        break;
+                    case 4:
+                        msec[Msec]+=number;
+                        Msec++;
+                        break;
+                    case 5:
+                        pressure[Pressure]+=number;
+                        Pressure++;
+                        break;
+                    case 6:
+                        PPGac[PPGAc]+=number;
+                        PPGAc++;
+                        break;
+                    case 7:
+                        PPGdc[PPGDc]+=number;
+                        PPGDc++;
+                        break;
+                    case 8:
+                        PPGraw[PPGRaw]+=number;
+                        PPGRaw++;
+                        break;
+                    default:
+                        break;
+                }
+                count1++;
+            }
+
+           /* StringBuilder displayText = new StringBuilder();
+            //int count2 = 0;
+            for (int number : PPGraw) {
+                displayText.append(number);
+                displayText.append("\n");
+            }
+            ResultText.setText(displayText.toString());*/
+            ArrayList<Entry> entries = new ArrayList<>();
+            for (int i = 0; i < msec.length; i++) {
+                entries.add(new Entry(msec[i],PPGdc[i]));
+            }
+            LineDataSet dataSet = new LineDataSet(entries, "PPGdc");
+            dataSet.setColor(Color.BLUE);
+            dataSet.setDrawCircles(false);
+            dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            LineData lineData = new LineData(dataSet);
+
+// グラフにデータを設定
+            ChartPPGdc.setData(lineData);
+            ChartPPGdc.setDrawMarkers(true);
+// グラフの更新
+            ChartPPGdc.invalidate();
+
+
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (Exception ex) {
+            // その他の例外処理
+            ex.printStackTrace();
+            showErrorMessage("エラー: " + ex.getMessage());
         }
+    }
+    private void showErrorMessage(String message) {
+        // エラーメッセージをユーザーに表示する方法を実装してください。
+        // 例えば、ダイアログボックス、Toastメッセージ、またはTextViewにエラーメッセージを表示できます。
+        // 以下は、Toastメッセージを表示する例です。
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
